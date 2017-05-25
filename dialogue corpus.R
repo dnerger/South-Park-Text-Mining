90cc62242953591f835b2f9f0abc33dacbe320d7
dnerger <dominiknerger@gmail.com>
2017-05-25 22:45:38 +0200

    worked on sentiment analysis / ner tagging

--- dialogue corpus.R
+++ dialogue corpus.R
@@ -17,9 +17,20 @@ by.episode <- NULL
 for(g in seq_along(episodes)){
   subset <- dialogue[dialogue$ES==episodes[g], ]
   subset <- subset[complete.cases(subset), ]
-  text <- str_c(subset$Text, collapse=" ")
+  text <- str_c(subset$Line, collapse=" ")
   row <- data.frame(episode.number=g, episode.code=episodes[g], season=subset$Season[1], text=text)
   by.episode <- rbind(by.episode, row)
+}
+
+seasons <-unique(dialogue$Season)
+seasons <-seasons[order(seasons)]
+by.season <- NULL;
+for(g in seq_along(seasons)){
+  subset <- dialogue[dialogue$Season==seasons[g], ]
+  subset <- subset[complete.cases(subset), ]
+  text <- str_c(subset$Line, collapse=" ")
+  row <- data.frame( season=seasons[g], text=text)
+  by.season <- rbind(by.season, row)
 }
 
 # create corpus
@@ -47,3 +58,54 @@ count.ep <- data.frame(inspect(ep.tdm.70))
 count.ep$word <- row.names(count.ep)
 
 write.csv(count.ep, "southpark_tdm_episode.csv", row.names=FALSE)
+
+
+dialogue <- read.csv('all-seasons.csv',stringsAsFactors = FALSE)
+full_dialogue = paste(by.season$text[18], collapse = " ")
+
+library(NLP)
+library(openNLP)
+library(magrittr)
+install.packages('rJava')
+
+full_dialogue <-as.String(full_dialogue)
+full_dialogue <- substr(full_dialogue,1,100000)
+word_ann <- Maxent_Word_Token_Annotator()
+sent_ann <- Maxent_Sent_Token_Annotator()
+
+dialogue_annotations <- annotate(full_dialogue, list(sent_ann, word_ann))
+
+class(dialogue_annotations)
+head(dialogue_annotations)
+
+person_ann <- Maxent_Entity_Annotator(kind = "person")
+location_ann <- Maxent_Entity_Annotator(kind = "location")
+organization_ann <- Maxent_Entity_Annotator(kind = "organization")
+
+dialogue_doc <- AnnotatedPlainTextDocument(full_dialogue, dialogue_annotations)
+sents(dialogue_doc) %>% head(10)
+
+
+pipeline <- list(sent_ann,
+                 word_ann,
+                 person_ann,
+                 location_ann,
+                 organization_ann)
+
+dialogue_annotations <- annotate(full_dialogue, pipeline)
+dialogue_doc <- AnnotatedPlainTextDocument(full_dialogue, dialogue_annotations)
+
+# Extract entities from an AnnotatedPlainTextDocument
+entities <- function(doc, kind) {
+  s <- doc$content
+  a <- annotations(doc)[[1]]
+  if(hasArg(kind)) {
+    k <- sapply(a$features, `[[`, "kind")
+    s[a[k == kind]]
+  } else {
+    s[a[a$type == "entity"]]
+  }
+}
+
+entities(dialogue_doc, kind = "person")
+sents(dialogue_doc)
